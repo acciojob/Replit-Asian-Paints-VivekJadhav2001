e58b43d677b24e326ecbfc6370ad3e0b71a0b38a
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-19 13:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+        const resetBtn = document.getElementById("Reset");
+
+        changeBtn.addEventListener("click", () => changeColor());
+        resetBtn.addEventListener("click", () => resetColor());
+
+        function changeColor() {
+            resetColor(); // reset all first
+
+            const selectedBox = document.getElementById("block_id").value;
+            const assignedColor = document.getElementById("colour_id").value;
+
+            if (selectedBox < 1 || selectedBox > 9) {
+                return alert("Please enter a valid box number (1-9)");
+            }
+
+            document.getElementById(selectedBox).style.backgroundColor = assignedColor;
+        }
+
+        function resetColor() {
+            for (let i = 1; i <= 9; i++) {
+                document.getElementById(i.toString()).style.backgroundColor = "transparent";
+            }
+        }
